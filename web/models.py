b8de4b4dd5dee22e0faab4bc7947a5f50b6c3da5
hossein otrsh <boyyoung2020@gmail.com>
2019-07-25 12:30:39 +0430

    a little change in model

--- web/models.py
+++ web/models.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 
 class Expense (models.Model):
     text=models.CharField(max_length=255)
-    date=models.DateField()
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     def __str__(self):
-        return self.text
+        return "{}:{}".format(self.text,self.date)
 
 class Income (models.Model):
     text=models.CharField(max_length=255)
-    date=models.DateField()
-    user=models.ForeignKey(User)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     def __str__(self):
-        return self.text
+        return "{}:{}".format(self.text,self.date)
